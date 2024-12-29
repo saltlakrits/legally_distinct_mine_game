d38002eca2fd82e4958c8bc81b373c48edb3509d
saltlakrits <delicious.mint.chocolate@gmail.com>
2024-12-29 21:47:50 +0100

    refactoring globals

--- src/minefield.h
+++ src/minefield.h
@@ -12,6 +12,24 @@ struct tile {
   uint8_t count;
 };
 
+// whenever we easily want to pass some coords,
+// use this
+struct point {
+	int x;
+	int y;
+};
+
+// we constantly pass around these variables, so let's keep
+// them together
+struct dims {
+	const unsigned LINES;
+	const unsigned COLS;
+	int lines;
+	int cols;
+	int start_y;
+	int start_x;
+};
+
 // don't call directly
 void *new_minefield(int lines, int cols, int starty, int startx);
 
@@ -24,3 +42,5 @@ void uncover_surrounding_tiles(void *field, int lines, int cols, int y_coord,
 int count_surrounding_flags(void *field, int lines, int cols, int y_coord,
                             int x_coord);
 int has_won(void *field, int lines, int cols);
+void print_grid(struct ncplane *ncp, void *field, int *has_lost, int lines,
+                int cols, int start_y, int start_x);
